6981d9ccca7e4b1e51d2c2a1e2faed85b862779f
gyani123 <22ghanendra@gmail.com>
2026-04-05 06:04:59 +0000

    Deploying to gh-pages from @ gyani123/gyani123.github.io@8ba68504e77250cc3945eb6fa49b582aca7f0630 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Interface of Research and Engineering.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
